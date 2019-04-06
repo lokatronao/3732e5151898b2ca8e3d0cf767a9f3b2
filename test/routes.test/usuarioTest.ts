2e3dfe9ed71e55d6da13a964d616c10eb2920955
lokatronao <lokatronao@gmail.com>
2019-04-06 17:24:02 +0200

    Creación de pruebas

---
+++ test/routes.test/usuarioTest.ts
@@ -0,0 +1,20 @@
+let _chai = require('chai');
+let chaiHttp = require('chai-http');
+const expect = require('chai').expect;
+
+_chai.use(chaiHttp);
+const url= 'http://localhost:3000';
+
+describe('Obtener prueba: ',()=>{
+
+	it('Obtener prueba', (done) => {
+		_chai.request(url)
+			.get('/user/prueba')
+			.end( function(err: any,res: { body: any; }){
+				console.log(res.body)
+				expect(res).to.have.status(200);
+				done();
+			});
+	});
+
+});
