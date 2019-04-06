2e3363d93db9769086fe1b8fc32ec9c6036ee73a
lokatronao <lokatronao@gmail.com>
2019-04-06 16:28:04 +0200

    Creación de conexion con bd

--- index.ts
+++ index.ts
@@ -1,11 +1,24 @@
 
 import Server from './classes/server';
 import userRoutes from './routes/usuario';
+import mongoose from 'mongoose';
 
 const server = new Server();
 
 server.app.use('/user', userRoutes);
 
+
+//Conectar BD
+
+mongoose.connect('mongodb://api:apiConnect8@ds123753.mlab.com:23753/curso',{
+    useNewUrlParser: true, useCreateIndex: true
+}, (err) =>{
+    if(err) throw err;
+
+    console.log("Base de datos ONLINE")
+});
+
+//Levantar express
 server.start( () => {
     console.log(`Servidor corriendo en ${server.port}`);
 });
