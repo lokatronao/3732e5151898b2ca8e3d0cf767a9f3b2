aea7a5c25d0df0b1d53c2e5ee8bf907d062c856e
lokatronao <lokatronao@gmail.com>
2019-04-07 22:35:22 +0200

    Finalización de tarea
    Creado el servicio para registro de usuarios

--- index.ts
+++ index.ts
@@ -2,9 +2,15 @@
 import Server from './classes/server';
 import userRoutes from './routes/usuario';
 import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
 
 const server = new Server();
 
+// Body parser
+server.app.use(bodyParser.urlencoded({ extended:true}));
+server.app.use(bodyParser.json());
+
+//Rutas de la api
 server.app.use('/user', userRoutes);
 
 
